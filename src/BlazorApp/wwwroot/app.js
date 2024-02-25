f4ae0c9a9a4c4f5cfd460d04174bb1f7f8dc43a5
Jonathan Potts <jonathanpotts@outlook.com>
2024-02-24 18:40:46 -0600

    Finish implementing dark mode

---
+++ src/BlazorApp/wwwroot/app.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+const setTheme = () => {
+    document.documentElement.setAttribute(
+        "data-bs-theme",
+        window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+}
+
+setTheme();
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => {
+    setTheme();
+});
+
+Blazor.addEventListener("enhancedload", () => {
+    setTheme();
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+});
