22ae24eaea84e7ddab0def77d073eb4334e07611
Jonathan Potts <jonathanpotts@outlook.com>
2024-03-01 17:08:49 -0600

    Start creating more test projects

---
+++ tests/Application.Tests/Services/RecipeServiceUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using JonathanPotts.RecipeCatalog.Domain;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace JonathanPotts.RecipeCatalog.Application.Tests.Services;
+
+public sealed class RecipeServiceUnitTests : IDisposable
+{
+    private readonly SqliteConnection _connection;
+    private readonly DbContextOptions<RecipeCatalogDbContext> _contextOptions;
+
+    private RecipeCatalogDbContext CreateContext() => new(_contextOptions);
+
+    public RecipeServiceUnitTests()
+    {
+        _connection = new("Filename=:memory:");
+        _connection.Open();
+
+        _contextOptions = new DbContextOptionsBuilder<RecipeCatalogDbContext>()
+            .UseSqlite(_connection)
+            .Options;
+
+        var context = CreateContext();
+        context.Database.EnsureCreated();
+    }
+
+    public void Dispose()
+    {
+        _connection.Dispose();
+    }
+}
