65c7afaeb9eeebd8ab4ad2ccd8ed2ce2b89e7bd9
Jonathan Potts <jonathanpotts@outlook.com>
2024-03-08 09:41:26 -0600

    Remove DALL-E 2 image sizes

--- src/AI/BaseOpenAIImageGenerator.cs
+++ src/AI/BaseOpenAIImageGenerator.cs
@@ -13,8 +13,6 @@ public abstract class BaseOpenAIImageGenerator : IAIImageGenerator
     {
         var imageSize = size?.ToLower() switch
         {
-            "256x256" => ImageSize.Size256x256,
-            "512x512" => ImageSize.Size512x512,
             "1024x1024" => ImageSize.Size1024x1024,
             "1024x1792" => ImageSize.Size1024x1792,
             "1792x1024" => ImageSize.Size1792x1024,
