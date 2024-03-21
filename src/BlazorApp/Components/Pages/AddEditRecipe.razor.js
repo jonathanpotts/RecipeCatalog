f0be1e2759593c60cf552b2b28db840a9e96b552
Jonathan Potts <jonathanpotts@outlook.com>
2024-03-21 16:00:56 -0500

    Fix add cuisine modal

---
+++ src/BlazorApp/Components/Pages/AddEditRecipe.razor.js
@@ -0,0 +1,13 @@
+﻿const addCuisineModalElement = document.getElementById("addCuisineModal");
+
+if (addCuisineModalElement) {
+    const addCuisineModal = new bootstrap.Modal(addCuisineModalElement);
+
+    window.showAddCuisineModal = () => {
+        addCuisineModal.show();
+    }
+
+    window.hideAddCuisineModal = () => {
+        addCuisineModal.hide();
+    };
+}
